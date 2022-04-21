b4bea1da22d53dad57199f3c55324138d3e826a8
deveshDentsu <devesh.kumar@dentsu.com>
2022-04-21 19:52:43 +0530

    first commit

---
+++ deployment/settings/local.py
@@ -0,0 +1,12 @@
+from .base import *
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'myapp',
+    'whitenoise.runserver_nostatic',
+]
---
+++ deployment/settings/production.py
@@ -0,0 +1,5 @@
+from .base import *
+
+SECRET_KEY = 'django-insecure-fkbiqk3fleg%n)cv%9n)e$tkdrd_ywz9tlwf%8s6yro1!v8h84'
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
